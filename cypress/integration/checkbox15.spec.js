4cfc4f7563a527c5362fe683b3c493b1b5950024
Tony Côrtes <tonycortes.h@hotmail.com>
2022-07-05 17:36:43 -0300

    Create checkbox15.spec.js

---
+++ cypress/integration/checkbox15.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('RadioButton', () => {
+        cy.get('#formSexoFem')
+            .click()
+            .should('be.checked')
+        
+        cy.get('#formSexoMasc').should('not.be.checked')   
+        
+        cy.get("[name='formSexo']").should('have.length', 2)
+    })
+
+    it.only('Checkbox', () => {
+        cy.get('#formComidaPizza')
+            .click()
+            .should('be.checked')
+
+        cy.get('[name="formComidaFavorita"]').click({multiple:true})
+    })
+
+})

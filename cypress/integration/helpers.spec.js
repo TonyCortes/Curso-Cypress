faff6f26e68edb6e81c525bd20de49a69b4a054c
Tony Côrtes <tonycortes.h@hotmail.com>
2022-07-15 10:25:38 -0300

    Update helpers.spec.js

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -23,4 +23,22 @@ describe('Helpers...', () =>{
         cy.wrap(promise).then(ret => console.log(ret))
         cy.get('#buttonList').then(() => console.log('Encontrei o segundo botao'))
     })
+
+    //aula 26 'its'
+    it.only('Its...', () => {
+        const obj = { nome: 'User', idade: 20 }
+        cy.wrap(obj).should('have.property', 'nome', 'User')
+        cy.wrap(obj).its('nome').should('be.equal','User')
+
+        const obj2 = { nome: 'User', idade: 20, endereco: {rua: 'dos bobos'} }
+        cy.wrap(obj2).its('endereco').should('have.property','rua')
+        cy.wrap(obj2).its('endereco').its('rua').should('contain','bobos')
+        cy.wrap(obj2).its('endereco.rua').should('contain','bobos')
+
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.title().its('length').should('be.equal', 20)
+
+
+    })
 })
